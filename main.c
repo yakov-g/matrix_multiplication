387b16152a45d71dadafbd5d2f0c014fd072f0fc
Yakov Goldberg <yakov.goldberg@gmail.com>
2016-08-01 04:16:56 -0700

    Add random multiplication option

--- main.c
+++ main.c
@@ -48,19 +48,20 @@ main(int argc, char **argv)
    Matrix *mt1 = NULL, *mt2 = NULL, *mult = NULL;
 
    char *filename1 = NULL, *filename2 = NULL;
-   int help = 0;
+   int help = 0, random = 0;
    static struct option long_options[] =
      {
           {"input1", required_argument, 0, '1'},
           {"input2", required_argument, 0, '2'},
           {"output", required_argument, 0, 'o'},
           {"threads", required_argument, 0, 't'},
+          {"random", required_argument, 0, 'r'},
           {"help", no_argument, 0, 'h'},
           {0, 0, 0, 0}
      };
 
    int long_index = 0, opt;
-   while ((opt = getopt_long(argc, argv, "ho:t:", long_options, &long_index)) != -1)
+   while ((opt = getopt_long(argc, argv, "ho:t:r:", long_options, &long_index)) != -1)
      {
         switch (opt)
           {
@@ -85,6 +86,13 @@ main(int argc, char **argv)
                       n_threads = strtol(argv[optind - 1], NULL, 0);
                       break;
                    }
+           case 'r':
+                   {
+                      random = strtol(argv[optind - 1], NULL, 0);
+                      if (random <= 0)
+                         random = 0;
+                      break;
+                   }
            default:
                    {
                       help = 1;
@@ -92,9 +100,12 @@ main(int argc, char **argv)
           }
      };
 
-   if (!path_input1 || !path_input2)
+   if (!random)
      {
-        help = 1;
+        if (!path_input1 || !path_input2)
+          {
+             help = 1;
+          }
      }
 
    if (n_threads <= 0)
@@ -105,28 +116,32 @@ main(int argc, char **argv)
 
    if (help)
      {
-        printf("Usage: %s --input1 file1 --input2 file2 -t N [-h/--help] [-o OUTFILE]\n", argv[0]);
+        printf("Usage: %s --input1 file1 --input2 file2 -t N [-h/--help] [-o OUTFILE] [-r/--random N]\n", argv[0]);
         printf("       --help/-h Print this help.\n");
         printf("       --input1 Provide 1st input filename.\n");
         printf("       --input2 Provide 2nd input filename.\n");
         printf("       --output/-o Provide output filename.\n");
         printf("       --threads/-t Provide number of threads\n");
+        printf("       --random/-r Provide size for random square matrix\n");
         printf("\n");
         goto end;
      }
 
-   filename1 = _realpath_get(path_input1);
-   if (!filename1)
+   if (!random)
      {
-        printf("Input path 1: doesn't exist or not a file\n");
-        goto end;
-     }
+        filename1 = _realpath_get(path_input1);
+        if (!filename1)
+          {
+             printf("Input path 1: doesn't exist or not a file\n");
+             goto end;
+          }
 
-   filename2 = _realpath_get(path_input2);
-   if (!filename2)
-     {
-        printf("Input path 2: doesn't exist or not a file\n");
-        goto end;
+        filename2 = _realpath_get(path_input2);
+        if (!filename2)
+          {
+             printf("Input path 2: doesn't exist or not a file\n");
+             goto end;
+          }
      }
 
    if (path_output && _is_path_exist(path_output))
@@ -135,8 +150,16 @@ main(int argc, char **argv)
         goto end;
      }
 
-   mt1 = matrix_from_file_create(filename1);
-   mt2 = matrix_from_file_create(filename2);
+   if (!random)
+     {
+        mt1 = matrix_from_file_create(filename1);
+        mt2 = matrix_from_file_create(filename2);
+     }
+   else
+     {
+        mt1 = matrix_random_create(random, random);
+        mt2 = matrix_random_create(random, random);
+     }
 
    if (!mt1 || !mt2)
       goto end;
